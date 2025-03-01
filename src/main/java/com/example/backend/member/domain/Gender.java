e7c7cce8ea705984f7873d6b092b008defb99e2e
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-03-01 22:20:55 +0900

    :rocket: feat : Member엔티티 추가

---
+++ src/main/java/com/example/backend/member/domain/Gender.java
@@ -0,0 +1,7 @@
+package com.example.backend.member.domain;
+
+public enum Gender{
+    MALE,
+    FEMALE,
+    NONE
+}
--- src/main/java/com/example/backend/member/domain/Member.java
+++ src/main/java/com/example/backend/member/domain/Member.java
@@ -1,2 +1,47 @@
-package com.example.backend.member.domain;public class Member {
+package com.example.backend.member.domain;
+
+import com.example.backend.common.BaseEntity;
+import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class Member extends BaseEntity {
+
+    @Column(nullable = false)
+    private String name;
+
+    @Embedded
+    @Column(nullable = false)
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String phoneNumber;
+
+    @Column(unique = true, nullable = false)
+    private String loginId;
+
+    @Column(nullable = false)
+    private LocalDate birthdate;
+
+    @Column(nullable = false)
+    private String password;
+
 }
