67c16f64358bb7c228a739abbab53370b21891eb
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-03-05 14:45:21 +0900

    🚀 feat : 공통관리 요소 추가

--- src/main/java/com/example/backend/common/CommonElement.java
+++ src/main/java/com/example/backend/common/CommonElement.java
@@ -1,2 +1,20 @@
-package com.example.backend.common;public class CommonElement {
+package com.example.backend.common;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+public class CommonElement {
+
+    public static final long JWT_EXPIRATION_PERIOD = 1000 * 60 * 60L;
+
+
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 }
