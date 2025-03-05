02eb7c906a59fa08e82d48c5dcc61bcb6b589db8
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-03-05 14:43:14 +0900

    🚀 feat : 로그인 컨트롤러 추가

--- src/main/java/com/example/backend/login/controller/JwtLoginController.java
+++ src/main/java/com/example/backend/login/controller/JwtLoginController.java
@@ -1,2 +1,36 @@
-package com.example.backend.login.controller;public class JwtLoginController {
+package com.example.backend.login.controller;
+
+import com.example.backend.login.dto.JoinRequest;
+import com.example.backend.login.dto.LoginRequest;
+import com.example.backend.login.jwt.JWTUtil;
+import com.example.backend.login.service.LoginService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/jwt-login")
+public class JwtLoginController {
+
+    private final LoginService loginService;
+    private final JWTUtil jwtUtil;
+
+    @PostMapping("/join")
+    public ResponseEntity<String> join(@Valid @RequestBody JoinRequest request){
+        ResponseEntity<String> response = loginService.memberJoin(request);
+
+        return response;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request){
+        ResponseEntity<String> response = loginService.login(request);
+
+        return response;
+    }
 }
