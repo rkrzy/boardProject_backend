674bcc1c3692d046a1f3498568f1aab3f5e234bc
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-03-05 14:40:30 +0900

    🚀 feat : CustomUserDetailsService추가

--- src/main/java/com/example/backend/login/service/CustomUserDetailsService.java
+++ src/main/java/com/example/backend/login/service/CustomUserDetailsService.java
@@ -1,2 +1,28 @@
-package com.example.backend.login.service;public class CustomUserDetailsService {
+package com.example.backend.login.service;
+
+import com.example.backend.login.config.CustomUserDetails;
+import com.example.backend.member.domain.Member;
+import com.example.backend.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String memberName) throws UsernameNotFoundException{
+        Member member = memberRepository.findByLoginId(memberName);
+
+        if(member != null){
+            return new CustomUserDetails(member);
+        }
+
+        return null;
+    }
 }
