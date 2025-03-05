e1d395c9f2b84de33364dad95fface31f4d248c6
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-03-05 14:38:21 +0900

    🚀 feat : CustomUserDetails추가

--- src/main/java/com/example/backend/login/config/CustomUserDetails.java
+++ src/main/java/com/example/backend/login/config/CustomUserDetails.java
@@ -1,2 +1,60 @@
-package com.example.backend.login.config;public class CustomUserDetails {
+package com.example.backend.login.config;
+
+import com.example.backend.member.domain.Member;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    public CustomUserDetails(Member member){
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return "ROLE_" + member.getRole().name();
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getLoginId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
 }
