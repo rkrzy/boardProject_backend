f7a6af6aa3ebbb9f1784cd682a8782ea3ee1a18a
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-03-05 14:41:08 +0900

    🚀 feat : SecurityConfig 보안 설정 추가

--- src/main/java/com/example/backend/login/config/SecurityConfig.java
+++ src/main/java/com/example/backend/login/config/SecurityConfig.java
@@ -1,2 +1,64 @@
-package com.example.backend.login.config;public class SecurityConfig {
+package com.example.backend.login.config;
+
+import com.example.backend.login.Filter.JWTFilter;
+import com.example.backend.login.Filter.LoginFilter;
+import com.example.backend.login.jwt.JWTUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthenticationConfiguration configuration;
+    private final JWTUtil jwtUtil;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration )throws Exception{
+
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin((auth) -> auth.disable());
+
+        http
+                .httpBasic((auth -> auth.disable()));
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/jwt-login", "/jwt-login/", "/jwt-login/login", "/jwt-login/join").permitAll()
+                        .requestMatchers("/jwt-login/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+        http
+                .sessionManagement((session) -> session.
+                        sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(configuration),jwtUtil),
+                        UsernamePasswordAuthenticationFilter.class);
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+
+        http
+                .csrf((auth) -> auth.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
