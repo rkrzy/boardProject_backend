9019912501a41cf4c7f118659e207672d0cee44c
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-03-05 13:22:14 +0900

    🚀 feat : 로그인 회원가입 요청 DTO 추가

--- src/main/java/com/example/backend/login/dto/JoinRequest.java
+++ src/main/java/com/example/backend/login/dto/JoinRequest.java
@@ -1,2 +1,60 @@
-package com.example.backend.login.dto;public class JoinRequest {
+package com.example.backend.login.dto;
+
+import com.example.backend.common.Role;
+import com.example.backend.member.domain.Address;
+import com.example.backend.member.domain.Gender;
+import com.example.backend.member.domain.Member;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JoinRequest {
+
+
+    @NotBlank(message = "ID를 입력하세요.")
+    private String loginId;
+
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    private String password;
+    private String passwordCheck;
+
+    @NotBlank(message = "이름을 입력하세요.")
+    private String name;
+
+    @NotNull(message = "주소를 입력하세요.")
+    private Address address;
+
+    @NotNull(message = "성별을 입력하세요.")
+    private Gender gender;
+
+    @NotBlank(message = "이메일을 입력하세요.")
+    private String email;
+
+    @NotNull(message = "생일을 입력하세요")
+    private LocalDate birthdate;
+
+    @NotBlank(message = "전화번호를 입력하세요")
+    private String phoneNumber;
+    public Member toEntity(){
+        return Member.builder()
+                .loginId(this.loginId)
+                .password(this.password)
+                .name(this.name)
+                .role(Role.USER)
+                .address(this.address)
+                .phoneNumber(this.phoneNumber)
+                .gender(this.gender)
+                .email(this.email)
+                .birthdate(this.birthdate)
+                .build();
+    }
+
+
 }
--- src/main/java/com/example/backend/login/dto/LoginRequest.java
+++ src/main/java/com/example/backend/login/dto/LoginRequest.java
@@ -1,2 +1,15 @@
-package com.example.backend.login.dto;public class LoginRequest {
+package com.example.backend.login.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    private String loginId;
+    private String password;
 }
