1f2790658d0fe2a29f81c6776075ca1de0177989
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-03-06 13:58:47 +0900

    🚀 feat #1 : PostRepository저장

--- src/main/java/com/example/backend/post/repository/PostRepository.java
+++ src/main/java/com/example/backend/post/repository/PostRepository.java
@@ -1,2 +1,8 @@
-package com.example.backend.post.repository;public class PostRepository {
+package com.example.backend.post.repository;
+
+import com.example.backend.post.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
 }
