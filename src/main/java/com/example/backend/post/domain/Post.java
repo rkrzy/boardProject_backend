1cfe2167087862037be9e49783754a9c61e756bb
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-03-06 13:53:56 +0900

    🚀 feat : Post엔티티 추가

--- src/main/java/com/example/backend/post/domain/Post.java
+++ src/main/java/com/example/backend/post/domain/Post.java
@@ -1,2 +1,33 @@
-package com.example.backend.post.domain;public class Post {
+package com.example.backend.post.domain;
+
+import com.example.backend.Image.domain.Image;
+import com.example.backend.common.BaseEntity;
+import com.example.backend.common.EventType;
+import com.example.backend.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseEntity {
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EventType eventType;
 }
