6dfdf8078b2f753a894cc95961bd6d9e6423f2c5
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-03-08 12:25:25 +0900

    🚀 feat : controller 게시물 삭제 추가

--- src/main/java/com/example/backend/post/controller/PostController.java
+++ src/main/java/com/example/backend/post/controller/PostController.java
@@ -4,12 +4,8 @@ package com.example.backend.post.controller;
 import com.example.backend.post.dto.PostRequest;
 import com.example.backend.post.service.PostService;
 import lombok.AllArgsConstructor;
-import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -27,5 +23,10 @@ public class PostController {
             @RequestPart(value = "images", required = false)List<MultipartFile> images) {
 
         return postService.create(request, images);
+    }
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<String> deletePost(@PathVariable Long postId){
+
+        return postService.delete(postId);
     }
 }
