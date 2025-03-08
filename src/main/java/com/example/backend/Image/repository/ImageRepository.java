dcce14a2d61513dfaaf681089114095db1fcb54b
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-03-08 12:24:40 +0900

    🚀 feat : 리포지토리 delete추가

--- src/main/java/com/example/backend/Image/repository/ImageRepository.java
+++ src/main/java/com/example/backend/Image/repository/ImageRepository.java
@@ -1,8 +1,14 @@
 package com.example.backend.Image.repository;
 
 import com.example.backend.Image.domain.Image;
+import com.example.backend.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImageRepository extends JpaRepository<Image, Long> {
+import java.util.List;
 
+public interface ImageRepository extends JpaRepository<Image, Long> {
+    public List<Image> findAllByPost(Post post);
+
+
+    public int deleteAllByPost(Post post);
 }
